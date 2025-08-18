2d4225b9de06bf02faece2a92a966b1a46e1aa91
Prabhav Gupta <prabhavgupta05@gmail.com>
2025-08-18 13:35:57 +0530

    Binary search practice

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -3,11 +3,32 @@ import static java.lang.Boolean.TRUE;
 public class BinarySearch {
     public static void main(String[] args) {
 
-        //int[] arr = {2, 5, 7, 9, 12, 16, 20};
-        int[] arr = {20, 15, 10, 8, 5, 3};
+        int[] Asc = {2, 5, 7, 9, 12, 16, 20};
+        int[] Dsc = {20, 15, 10, 8, 5, 3};
 
-        int result = binarysrch(arr, 5);
+        System.out.println(Dsc.length);
+        int result = binarysrch(Dsc, 5);
+        int result2 = binarySearch(Asc, 7);
+        System.out.println("Ascending binary search : " + result2);
         System.out.println(result);
+    }
+
+    public static  int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while ( start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[mid] > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return  -1;
     }
 
     public static int binarysrch(int[] arr, int target){
