81c2dcca8166665a5fcc99a36a72a183f5f75c91
Prabhav Gupta <prabhavgupta05@gmail.com>
2026-02-05 19:27:32 +0530

    Added Trees

--- src/Tree/DFS.java
+++ src/Tree/DFS.java
@@ -240,8 +240,25 @@ Good Nodes = 3 → [3, 4, 5]
             node.right = dhelper(queue);
             return node;
         }
-
     }
+
+
+    // https://leetcode.com/problems/path-sum/
+    static class PathSum {
+        public boolean hasPathSum(TreeNode root, int sum) {
+            if(root == null)
+                return false;
+            boolean left = hasPathSum(root.left, sum - root.val);
+            boolean right = hasPathSum(root.right, sum - root.val);
+
+            if(root.val == sum && root.left == null && root.right == null)
+                return true;
+
+            return left || right;
+        }
+    }
+    
+
 
 
 
