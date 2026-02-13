a68d0d1f2a9cf2c3687879b3b483aa99373b69a3
Prabhav Gupta <prabhavgupta05@gmail.com>
2026-02-14 01:30:56 +0530

    sliding window

---
+++ src/SlidingWindow/FruitsIntoBasket.java
@@ -0,0 +1,33 @@
+package SlidingWindow;
+
+import java.util.HashMap;
+
+public class FruitsIntoBasket {
+    // https://leetcode.com/problems/fruit-into-baskets/
+    class Solution {
+        // Return max continuous subarray that contains at most two distinct elements, digits(fruits)
+        public int totalFruit(int[] fruits) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            int left = 0;
+            int max = 0;
+
+            for(int right = 0; right<fruits.length ; right++) {
+                map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
+
+                while(map.size() > 2) {
+                    map.put(fruits[left], map.get(fruits[left])-1);
+
+                    if (map.get(fruits[left]) == 0)
+                        map.remove(fruits[left]);
+
+                    left++;
+
+                }
+
+                max = Math.max(max, right - left + 1);
+
+            }
+            return max;
+        }
+    }
+}
