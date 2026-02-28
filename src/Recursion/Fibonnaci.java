159b05bf09e516ae0ac73f6df78c069f1a7d2604
Prabhav Gupta <prabhavgupta05@gmail.com>
2026-02-28 20:17:19 +0530

    update PR

---
+++ src/Recursion/Fibonnaci.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+public class Fibonnaci {
+
+    // https://leetcode.com/problems/fibonacci-number/description/
+    public int fib(int n) {
+        // base condition
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+        // Induction step
+        return fib(n-1) + fib(n-2);
+    }
+
+
+    // This is for finding the factorial of a number using recursion
+    public int factorial(int n) {
+        if(n == 1)
+            return 1;
+        return n * factorial(n-1);
+    }
+}
