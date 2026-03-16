768463d5503148bd8a4dbc84b7044f9fd7d2ad30
Prabhav Gupta <prabhavgupta05@gmail.com>
2026-03-16 16:00:44 +0530

    update PR

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -10,10 +11,13 @@ public class Main {
         String nextLine = input.next().trim();
         System.out.println(nextLine);
 
+        int[] arr = {1, 2, 3, 4, 5};
+
         ArrayList<Integer> list = new ArrayList<>();
         list.add(78);
         list.add(90);
-        Iterator<Integer> iterator = list.iterator(); // we are creating an iterator to iterate through the list, we can also use a for-each loop to iterate through the list, but here we are using an iterator to demonstrate how to use it
+        Iterator<Integer> iterator = list.iterator();
+        Iterator<Integer> itr = Arrays.stream(arr).iterator(); // we are creating an iterator for the array arr using the Arrays.stream(arr).iterator() method, which allows us to iterate through the elements of the array using the iterator's method, but why
         while(iterator.hasNext()) {
             System.out.println(iterator.next()); // we are using the next() method of the iterator to get the next element in the list, and we are printing it out
         }
