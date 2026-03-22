6a85b1661b169ab69c5364400d37afd306bac432
Prabhav Gupta <prabhavgupta05@gmail.com>
2026-03-22 17:52:22 +0530

    update PR

--- src/SlowFastPointer/HappyNumber.java
+++ src/SlowFastPointer/HappyNumber.java
@@ -1,5 +1,7 @@
 package SlowFastPointer;
 
+import java.util.HashSet;
+
 // https://leetcode.com/problems/happy-number/
 public class HappyNumber {
 
@@ -23,5 +25,36 @@ public class HappyNumber {
             num /= 10;
         }
         return ans;
+    }
+
+
+    // Using Set
+
+    public boolean isHappy2(int n) {
+        // First create a set to check if that number is visited or not, if not proceed
+        // as if is visited it will be in a loop so not happy number
+        HashSet<Integer> set = new HashSet<>();
+
+        while(!set.contains(n)) { // We are iterating for every n which calc calculates, which is not present, it will stop when n becomes n or n is present in the set
+            set.add(n);
+            n = calc(n);
+
+            if(n == 1)
+                return true;
+        }
+        return false;
+    }
+
+    // create a function to calculate the sum of digits and return the number
+    public int calc(int n) {
+        int sum = 0;
+
+        while(n>0) {
+            int digit = n % 10;
+            sum = sum + digit * digit;
+            n = n / 10;
+        }
+
+        return sum;
     }
 }
